01099d45965782b3278103846e30920a61c6e58c
Joe Cooper <joe@artofbytes.com>
2015-07-31 15:01:58 +0200

    Added reference to sample sentence to word

--- SpellingKitUnity/Word.cs
+++ SpellingKitUnity/Word.cs
@@ -6,9 +6,16 @@ namespace NobleMuffins.SpellingKit.Unity {
 	public class Word : ScriptableObject, IWord {
 
 		public AudioClip utterance;
+		public SampleSentence[] sampleSentences;
 		public Spelling[] spellings;
 
 		#region IWord implementation
+		public IEnumerable<ISampleSentence> SampleSentences {
+			get {
+				return sampleSentences;
+			}
+		}
+
 		public IEnumerable<ISpelling> Spellings {
 			get {
 				return spellings;
